771f7e46a6be33cf377e38fc5417aaa96f2acc3d
Ramana Giri <loamramg@gmail.com>
2025-10-09 10:06:52 +0530

    delete resume docx

--- src/main/java/com/example/recruitment/RecruitmentPlatformApplication.java
+++ src/main/java/com/example/recruitment/RecruitmentPlatformApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecruitmentPlatformApplication {
 
     public static void main(String[] args) {
-        SpringApplication   .run(RecruitmentPlatformApplication.class, args);
+        SpringApplication.run(RecruitmentPlatformApplication.class, args);
     }
 
 }
